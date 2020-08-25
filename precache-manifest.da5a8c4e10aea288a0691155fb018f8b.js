6c2f2972cb8b45ee0cd55d4476871b14b9b85099
Robertas <robertas.atko@gmail.com>
2020-08-25 17:01:44 +0300

    Updates

---
+++ precache-manifest.da5a8c4e10aea288a0691155fb018f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cbcd6b6e6a6672d104dc5e3c4d9848",
+    "url": "/chuck/index.html"
+  },
+  {
+    "revision": "5d876c496ce416d4e467",
+    "url": "/chuck/static/css/main.ab72851f.chunk.css"
+  },
+  {
+    "revision": "2edc73462766f060f5b8",
+    "url": "/chuck/static/js/2.c29acc81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuck/static/js/2.c29acc81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d876c496ce416d4e467",
+    "url": "/chuck/static/js/main.b1462af0.chunk.js"
+  },
+  {
+    "revision": "cbb70fdb6f4daba8405f",
+    "url": "/chuck/static/js/runtime-main.3b11d4e6.js"
+  }
+]);
